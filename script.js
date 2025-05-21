c85df40e5a4f67ff887b773ded01966a1c442ffa
arslnbusra <147207378+arslnbusra@users.noreply.github.com>
2025-05-21 09:46:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-function updateTime() {
+function updateClockAndDate() {
   const now = new Date();
+
+  // Saat
   const hours = String(now.getHours()).padStart(2, '0');
   const minutes = String(now.getMinutes()).padStart(2, '0');
   const seconds = String(now.getSeconds()).padStart(2, '0');
-  document.getElementById('time').textContent = `${hours}:${minutes}:${seconds}`;
+  document.getElementById("clock").textContent = `${hours}:${minutes}:${seconds}`;
+
+  // Tarih
+  const days = ['Pazar','Pazartesi','Salı','Çarşamba','Perşembe','Cuma','Cumartesi'];
+  const months = ['Ocak','Şubat','Mart','Nisan','Mayıs','Haziran','Temmuz','Ağustos','Eylül','Ekim','Kasım','Aralık'];
+  const day = now.getDate();
+  const month = months[now.getMonth()];
+  const year = now.getFullYear();
+  const weekday = days[now.getDay()];
+  document.getElementById("date").textContent = `${day} ${month} ${year} ${weekday}`;
 }
 
-setInterval(updateTime, 1000);
-updateTime();
+setInterval(updateClockAndDate, 1000);
+updateClockAndDate();
